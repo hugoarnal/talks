fcd046ad58709f68706394d9d1034b39c2f1f7e4
Hugo ARNAL <hugo@hugoarnal.com>
2026-07-07 11:46:59 +0200

    slides-builder: apply basedpyright comments

--- slides-builder/main.py
+++ slides-builder/main.py
@@ -2,13 +2,14 @@ import os
 import shutil
 from enum import Enum
 from pathlib import Path
+from typing import override
 
 class LineType(Enum):
     Compilation = "compilation"
     Include = "include"
 
     @staticmethod
-    def construct(line: str) -> Line | None:
+    def construct(line: str) -> Line:
         factory = {
             LineType.Compilation: CompilationLine,
             LineType.Include: IncludeLine
@@ -19,15 +20,17 @@ class LineType(Enum):
             if enum.value == arguments[0]:
                 return factory[enum](arguments)
 
+        raise Exception("Invalid type: ", arguments[0])
+
 class Line:
-    def __init__(self, arguments):
-        self.type = None
-        self.arguments = arguments
+    def __init__(self, lineType: LineType, arguments: list[str]) -> None:
+        self.type: LineType = lineType
+        self.arguments: list[str] = arguments
 
     def getType(self) -> LineType:
         return self.type
 
-    def execute(self, basePath: Path, lines: list[str]) -> None:
+    def execute(self, basePath: Path, lines: list[str]) -> None: # pyright: ignore[reportUnusedParameter]
         pass
 
     @staticmethod
@@ -35,33 +38,33 @@ class Line:
         return line.split(":")
 
 class CompilationLine(Line):
-    def __init__(self, arguments):
-        super().__init__(arguments)
-        self.type = LineType.Compilation
+    def __init__(self, arguments: list[str]):
+        super().__init__(LineType.Compilation, arguments)
 
+    @override
     def execute(self, basePath: Path, lines: list[str]) -> None:
-        os.system(f"make -C {basePath}/{self.arguments[1]} {self.arguments[2]}")
+        _ = os.system(f"make -C {basePath}/{self.arguments[1]} {self.arguments[2]}") # pyright: ignore[reportDeprecated]
 
 class IncludeLine(Line):
-    def __init__(self, arguments):
-        super().__init__(arguments)
-        self.type = LineType.Include
+    def __init__(self, arguments: list[str]):
+        super().__init__(LineType.Include, arguments)
 
+    @override
     def execute(self, basePath: Path, lines: list[str]) -> None:
         lines.append(self.arguments[1])
 
 class Configuration:
     def __init__(self, fileName: str) -> None:
-        self.COMMENT_SIGN = "#"
+        self.COMMENT_SIGN: str = "#"
 
-        self.fileName = fileName
-        self.rawLines = []
+        self.fileName: str = fileName
+        self.rawLines: list[str] = []
 
     def read(self) -> list[Line]:
         with open(self.fileName, "r") as f:
             self.rawLines = f.read().splitlines()
 
-        toRemove = []
+        toRemove: list[str] = []
         for _, line in enumerate(self.rawLines):
             # For now, comments are only parsed if they are starting with # sign
             if line.startswith(self.COMMENT_SIGN):
@@ -87,17 +90,17 @@ class Configuration:
 
 class Builder:
     def __init__(self):
-        self.buildPath = Path("build")
-        self.slidesPath = Path(self.buildPath / "slides")
-        self.basePath = Path("..")
-        self.templatesPath = Path("templates")
-        self.config = Configuration(self.basePath / "slides")
-        self.files = []
+        self.buildPath: Path = Path("build")
+        self.slidesPath: Path = Path(self.buildPath / "slides")
+        self.basePath: Path = Path("..")
+        self.templatesPath: Path = Path("templates")
+        self.config: Configuration = Configuration(str(self.basePath / "slides"))
+        self.files: list[str] = []
 
         self._createFolder(Path(self.buildPath).parent)
         self._createFolder(Path(self.slidesPath).parent)
 
-    def _createFolder(self, path):
+    def _createFolder(self, path: Path) -> None:
         if not os.path.exists(path):
             os.makedirs(path)
 
@@ -108,7 +111,7 @@ class Builder:
     def _copyToBuildFolder(self):
         for file in self.files:
             self._createFolder(Path(self.slidesPath / file).parent)
-            shutil.copyfile(self.basePath / file, self.slidesPath / file)
+            _ = shutil.copyfile(self.basePath / file, self.slidesPath / file)
 
     def execute(self):
         self._executeConfig()
